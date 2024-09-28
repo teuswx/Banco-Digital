8cdfd30849726a19c3964a50d7b379b6ca265195
teuswx <mateushenriqpereira02@gmail.com>
2024-09-28 11:29:39 -0300

    atualização final

--- src/Banco.java
+++ src/Banco.java
@@ -1,64 +1,82 @@
-import java.util.ArrayList;
-import java.util.List;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Banco {
-    private String nome;
-    private List<Conta> contas;
+    private  static final String  NOME= "Banco do Brasil";
+    private Map<Integer, Conta> contas;
 
-    public String getNome() {
-        return nome;
-    }
-
-    public List<Conta> getContas() {
+    
+    public Map<Integer, Conta> getContas() {
         return contas;
     }
 
-    public void setContas(List<Conta> contas) {
+    public void setContas(Map<Integer, Conta> contas) {
         this.contas = contas;
+    }
+    
+    public String getNome() {
+        return NOME;
     }
 
     public Banco() {
-        this.contas = new ArrayList<>();
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
+        this.contas = new HashMap<>();
     }
 
     public void adicionarConta(Cliente cliente, String tipoConta) {
         if (tipoConta == "corrente") {
             Conta conta = new ContaCorrente(cliente);
-            this.contas.add(conta);
+            this.contas.put(conta.numero, conta);
         } else if (tipoConta == "poupanca") {
             Conta conta = new ContaPoupanca(cliente);
-            this.contas.add(conta);
+            this.contas.put(conta.numero, conta);
         }
     }
 
     public void removerConta(int numero){
         if(!contas.isEmpty()){
-            for(Conta i : contas){
-                if(i.getNumero() == numero){
-                    contas.remove(i);
-                }
-            }
+          contas.remove(numero);
         }else{
             System.out.println("\n!Não há contas cadastradas!");
         }
     }
 
+    public Conta pesquisarPorNumero(int numero){
+        if(contas.isEmpty()){
+            System.out.println("\n!Não há contas cadastradas!");
+            return null;
+        }else if(contas.get(numero) == null){
+            System.out.println("\n!Conta não encontrada!");
+            return null;
+        }else{
+            return contas.get(numero);
+        }
+      
+    }
+
+    public void depositar(int numero, double valor){
+        pesquisarPorNumero(numero).depositar(valor);
+    }
+
+    public void sacar(int numero, double valor){
+        pesquisarPorNumero(numero).sacar(valor);
+    }
+
+    public void transferir(int numContaSaida, double valor, int numContaDestino){
+        pesquisarPorNumero(numContaSaida).sacar(valor);
+        pesquisarPorNumero(numContaDestino).depositar(valor);
+
+    }
     public void exibirContas() {
         if (!contas.isEmpty()) {
-          System.out.println(this.contas);
+          for(Conta  c: contas.values()){
+            c.imprimirExtrato();
+          }
         } else {
           System.out.println("!Não há contas cadastradas!");
         }
       }
-    
-    @Override
-    public String toString() {
-        return "Banco [nome=" + nome + ", contas=" + contas + "]";
-    }
 
+  
     
 }
--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,108 @@
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        Cliente mateus = new Cliente();
-        mateus.setNome("Mateus");
-        Cliente pedro = new Cliente();
-        pedro.setNome("Pedro");
+    public static void main(String[] args) {   
+        char c = ' ';
+        Scanner scanner = new Scanner(System.in);
+        Banco banco = new Banco();
+        while (c != 's') {
+            do {
+                System.out.println("\nDigite uma letra para realizar sua respectiva função: \na-Cadastrar conta\nb-Depositar\nc-Sacar\nd-Trasferir saldo\ne-Excluir conta\nf-Exibir contas\ns-Sair");
+                c = scanner.next().charAt(0);
+              
+                if(c != 'a' && c != 'b' && c != 'c' && c != 'd' && c != 'e' && c != 'f'  && c != 's'){
+                    System.out.println("\n!Digite uma opção válida!\n");
+                }     
+            } while (c != 'a' && c != 'b' && c != 'c' && c != 'd' && c != 'e' && c != 'f' && c != 's');
+       
 
-        
-        Banco b = new Banco();
-        b.adicionarConta(pedro, "corrente");
-        b.adicionarConta(mateus, "poupanca");
+            switch (c) {
+                case 'a':
+                    
+                    System.out.println("\nDigite o nome do cliente: ");
+                    scanner.nextLine();
+                    String nome = scanner.nextLine();
+                    Cliente cliente = new Cliente(nome);
+                    String tipoContaAux ="";
+                    char tipoConta = 'c';
+                    do {
+                        System.out.println("\nSelecione uma opção de conta:\na-Conta corrente\nb-conta poupança");
+                        tipoConta = scanner.nextLine().charAt(0);
+                        if (tipoConta == 'a') {
+                            tipoContaAux = "corrente";
+                        } else if (tipoConta == 'b') {
+                            tipoContaAux = "poupanca";
+                        }
+                       
+                        if(tipoConta != 'a' && tipoConta != 'b' ){
+                            System.out.println("\n!Digite uma opção válida!\n");
+                        }     
+                    } while (tipoConta != 'a' && tipoConta != 'b' );
+                    
+                    System.out.println(cliente);
+                    System.out.println(tipoContaAux);
+                    banco.adicionarConta(cliente, tipoContaAux);
+                   
+                    break;
+                case 'b':
+                    int numeroConta = 0;
+                    double valor = 0;
 
-        b.exibirContas();
-    
-        b.removerConta(1);
-        b.exibirContas();
+                    System.out.println("\nDigite o número da conta: ");
+                    scanner.nextLine();
+                    numeroConta = scanner.nextInt();
+                    System.out.println("\nDigite o valor do depósito: ");
+                    scanner.nextLine();
+                    valor = scanner.nextDouble();
+
+                    banco.depositar(numeroConta, valor);
+                    break;
+                case 'c':
+                    numeroConta = 0;
+                    valor = 0;
+
+                    System.out.println("\nDigite o número da conta: ");
+                    scanner.nextLine();
+                    numeroConta = scanner.nextInt();
+                    System.out.println("\nDigite o valor de saque: ");
+                    scanner.nextLine();
+                    valor = scanner.nextDouble();
+
+                    banco.sacar(numeroConta, valor);
+                    break;
+                case 'd':
+                int numeroContadestino = 0;
+                numeroConta = 0;
+                valor = 0;
+
+                System.out.println("\nDigite o número da conta de saída: ");
+                scanner.nextLine();
+                numeroConta = scanner.nextInt();
+                System.out.println("\nDigite o número da conta de destino: ");
+                scanner.nextLine();
+                numeroContadestino = scanner.nextInt();
+                System.out.println("\nDigite o valor da transferência: ");
+                scanner.nextLine();
+                valor = scanner.nextDouble();
+
+                banco.transferir(numeroConta, valor, numeroContadestino);
+                    break;
+                case 'e':
+                    numeroConta = 0;
+                    System.out.println("\nDigite o número da conta: ");
+                    scanner.nextLine();
+                    numeroConta = scanner.nextInt();
+                    banco.removerConta(numeroConta);
+                    break;
+                case 'f':
+                    banco.exibirContas();
+                    break;
+                case 's':
+                    break;
+                default:
+                    break;
+            }
+        }
+        scanner.close();
     }
 }
